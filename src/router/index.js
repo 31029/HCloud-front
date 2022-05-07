e641eb98b38af0587fbd74e7ebf0c691dc68bc07
31029 <740876073@qq.com>
2022-05-07 22:22:43 +0800

    v2 -  初级版mycloud

--- src/router/index.js
+++ src/router/index.js
@@ -74,88 +74,43 @@ export const constantRoutes = [
         component: () => import('@/views/workloads/podslist'),
         meta: { title: 'Pods列表', icon: 'tree' }
       },
-      {
-        path: 'serivces',
-        name: 'Serivces',
-        component: () => import('@/views/workloads/svclist'),
-        meta: { title: 'Service列表', icon: 'tree' }
-      }
-    ]
-  },
-
-
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
+      
     ]
   },
 
   {
-    path: '/nested',
+    path: '/nodes',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
+    redirect: '/nodes/nodeshell',
+    name: 'nodes',
+    meta: { title: '节点监控', icon: 'el-icon-s-help' },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'nodeshell',
+        name: 'Nodeshell',
+        component: () => import('@/views/nodes/nodeshell'),
+        meta: { title: 'nodeshell(临时)', icon: 'table' },
+        hidden: true
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
-      }
+        path: 'nodelist',
+        name: 'Nodelist',
+        component: () => import('@/views/nodes/nodelist'),
+        meta: { title: '节点列表', icon: 'tree' }
+      },
+      {
+        path: 'nodedetail',
+        name: 'Nodedetail',
+        component: () => import('@/views/nodes/nodedetail'),
+        meta: { title: '节点编辑', icon: 'tree' },
+        hidden: true
+      },
     ]
   },
 
+ 
+
+  
   {
     path: 'external-link',
     component: Layout,
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">当前用户： {{ name }}</div>
+    <p>毕业设计项目 ： MyCloud</p>
+    <p>@中南林业科技大学 - - - - - 18级软件工程一班  - - - - - -  - 黄卓远 -  学号：20183353</p>
   </div>
 </template>
 
